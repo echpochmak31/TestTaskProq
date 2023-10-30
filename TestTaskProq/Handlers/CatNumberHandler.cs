8d314b6ff8ea939275037272cefba9bce3d9cc6f
echpochmak31 <echpochmak83@gmail.com>
2023-10-30 17:39:45 +0300

    feat: add new handlers

---
+++ TestTaskProq/Handlers/CatNumberHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace TestTaskProq.Handlers;
+
+public class CatNumberHandler : BasicNumberHandler
+{
+    public override void Handle(INumber number)
+    {
+        if (number.Value % 5 == 0)
+        {
+            number.Substitutions.Add("cat");
+        }
+        base.Handle(number);
+    }
+}
---
+++ TestTaskProq/Handlers/DogNumberHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace TestTaskProq.Handlers;
+
+public class DogNumberHandler : BasicNumberHandler
+{
+    public override void Handle(INumber number)
+    {
+        if (number.Value % 3 == 0)
+        {
+            number.Substitutions.Add("dog");
+        }
+        base.Handle(number);
+    }
+}
