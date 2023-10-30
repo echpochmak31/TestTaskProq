6c14cb28c0c7446f8c29a62d12c6d2a1fb11c831
echpochmak31 <echpochmak83@gmail.com>
2023-10-30 17:39:28 +0300

    feat: add new formatter

---
+++ TestTaskProq/Format/GoodBoyFormatter.cs
@@ -0,0 +1,12 @@
+﻿namespace TestTaskProq.Format;
+
+public class GoodBoyFormatter : IFormatter
+{
+    public string FormatSubstitutions(IEnumerable<string> substitutions)
+    {
+        if (!substitutions.Contains("dog") || !substitutions.Contains("cat")) return string.Join('-', substitutions);
+        var modified = new List<string>() {"good-boy"};
+        modified.AddRange(substitutions.Where(x => !x.Equals("dog") && !x.Equals("cat")).ToList());
+        return string.Join('-', modified);
+    }
+}
