d37d68eef61a1819f092948b2f26ea4405254315
echpochmak31 <echpochmak83@gmail.com>
2023-10-30 17:18:49 +0300

    feat: add handlers for division by 4 and 7

---
+++ TestTaskProq/Handlers/DivisibleBy4Handler.cs
@@ -0,0 +1,13 @@
+﻿namespace TestTaskProq.Handlers;
+
+public class DivisibleBy4Handler : BasicNumberHandler
+{
+    public override void Handle(INumber number)
+    {
+        if (number.Value % 4 == 0)
+        {
+            number.Substitutions.Add("muzz");
+        }
+        base.Handle(number);
+    }
+}
---
+++ TestTaskProq/Handlers/DivisibleBy7Handler.cs
@@ -0,0 +1,13 @@
+﻿namespace TestTaskProq.Handlers;
+
+public class DivisibleBy7Handler : BasicNumberHandler
+{
+    public override void Handle(INumber number)
+    {
+        if (number.Value % 7 == 0)
+        {
+            number.Substitutions.Add("guzz");
+        }
+        base.Handle(number);
+    }
+}
